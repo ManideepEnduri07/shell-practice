05a33440a60207f468d8df9d44c9339cdaaae5dc
Manideep <manideepmani1998@gmail.com>
2026-02-04 22:33:52 +0530

    Variables code added

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+Trump:: Hey Musk, How are you?
+Musk:: Hello Trump, I am fine. How are you doing
+Trump:: I am fine too. What's up?
+Musk:: Nothing, just going to Mars now, will you come?
+Trump:: Sorry, you carry on! I will come once you come back
