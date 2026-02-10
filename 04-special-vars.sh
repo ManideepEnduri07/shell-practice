84e672a4e26fab29ca0525055f5d387262eaac2a
Manideep <manideepmani1998@gmail.com>
2026-02-10 19:40:58 +0530

    Special_Variables

--- 04-special-vars.sh
+++ 04-special-vars.sh
@@ -3,4 +3,9 @@
 echo All variables passed to the script: $@
 echo Number of variables passed to the script: $#
 echo Name of the script: $0
-#echo PID of the script: $$
+echo Present working directory: $PWD
+echo Home directory: $HOME
+echo Current user: $USER
+echo PID of the script: $$
+sleep 10 &
+echo PID of the last background process: $!
